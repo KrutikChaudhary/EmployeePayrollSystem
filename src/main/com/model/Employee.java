744fb420f3a96c4a7288e7ff2a764f14dfd74473
yash roushan <yashroushan@gmail.com>
2023-11-17 23:27:37 -0400

    constructed employee class

--- src/main/com/model/Employee.java
+++ src/main/com/model/Employee.java
@@ -2,4 +2,39 @@ package main.com.model;
 
 public class Employee {
 
+    // instance variables
+    private int employeeId;
+    private String name;
+    private String designation;
+    private String department;
+
+    // constructor
+    public Employee(int employeeId, String name, String designation, String department) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.designation = designation;
+        this.department = department;
+    }
+
+    // accessors
+    public int getEmployeeId() {
+        return this.employeeId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDesignation() {
+        return this.designation;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    // mutators
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 }
